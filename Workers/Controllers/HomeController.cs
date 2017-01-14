9fc922d3dac93c5e6a609a41ad63c9b2cef87843
Oleg <pozitiv862@gmail.com>
2017-01-14 22:31:05 +0200

    Test Project Add

---
+++ Workers/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Workers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using(var db = new WorkersdbEntities())
+            {
+                var workers = db.Workers.ToList();
+                return View(workers);
+            }            
+        }
+
+        [HttpGet]
+        public ActionResult AddWorker()
+        {
+            WorkersdbEntities db = new WorkersdbEntities();
+            List<Worker> worker = db.Workers.ToList();
+            ViewBag.WorkersList = new SelectList(worker, "Id", "Firstname");
+            return PartialView("_AddWorker");
+        }
+
+        [HttpPost]
+        public ActionResult AddWorker(Worker worker)
+        {
+            if(worker.Firstname != null && worker.Surname !=null && worker.Patronymic != null && worker.Position != null)
+            {
+                worker.Id = Guid.NewGuid();
+                using (var db = new WorkersdbEntities())
+                {
+                    db.Workers.Add(worker);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+          
+        }
+
+        [HttpGet]
+        public ActionResult EditWorker(Guid id)
+        {
+            WorkersdbEntities db = new WorkersdbEntities();
+            List<Worker> workerlist = db.Workers.ToList();
+            ViewBag.WorkersList = new SelectList(workerlist, "Id", "Firstname");
+            Worker worker = db.Workers.Find(id);
+
+            return PartialView("_EditWorker", worker);
+        }
+
+        [HttpPost]
+        public ActionResult EditWorker(Worker worker)
+        {
+            if(worker.Firstname != null && worker.Surname != null && worker.Patronymic != null && worker.Position != null)
+            {
+                using (var db = new WorkersdbEntities())
+                {
+                    db.Entry(worker).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            } else { return RedirectToAction("Index"); }
+            
+        }
+
+        [HttpGet]
+        public ActionResult DeleteWorker(Guid id)
+        {
+            WorkersdbEntities db = new WorkersdbEntities();
+            Worker worker = db.Workers.Find(id);
+            return PartialView("_DeleteWorker" , worker);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteWorker(Worker worker)
+        {
+            WorkersdbEntities db = new WorkersdbEntities();
+
+            db.Workers.Attach(worker);
+            db.Workers.Remove(worker);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Workers/Models/Worker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Workers
+{
+    public partial class Worker
+    {
+        public string ChiefName { get
+            {
+                if(ChiefId!= null && ChiefId != Guid.Empty)
+                {
+                    using (var db = new WorkersdbEntities())
+                    {
+                        return db.Workers.First(item => item.Id == ChiefId).Firstname;
+                    }
+                }else
+                {
+                    return "";
+                }
+                
+            }
+        }
+
+    }
+}
